2efbd5db11ed3b8fde4c49ae5b5bfb7d66b84714
kuban83 <kuban83@inbox.ru>
2016-03-03 20:23:31 +0200

    Add Before and After methods Into TestRunner.java class

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,10 +1,22 @@
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /**
  * Created by Анна on 03.03.2016.
  */
 public class TestRunner {
+    @BeforeMethod
+    public void ThisBeforeTestMethod(){
+        System.out.println( " This is before testMethod");
+    }
+    @AfterMethod
+    public void ThisAfterTestMethod (){
+        System.out.println("   This is after test method");
+    }
+
     @Test
+
     public void ThisIsTestMethod1 (){
         System.out.println("This Is Test Method 1");
     }
