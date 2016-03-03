a84754e9385d1979def158605a039c4432fadc93
kuban83 <kuban83@inbox.ru>
2016-03-03 20:37:50 +0200

    Add BesoreTest annotation and Method

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,11 +1,16 @@
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 /**
  * Created by Анна on 03.03.2016.
  */
 public class TestRunner {
+@BeforeTest
+public void ThisIsBeforeTest(){
+    System.out.println("This is out test case (before)");
+}
     @BeforeMethod
     public void ThisBeforeTestMethod(){
         System.out.println( " This is before testMethod");
