4e825f65587c0e2f6bb4edfdc319a65530320868
kuban83 <kuban83@inbox.ru>
2016-03-03 20:06:39 +0200

    Initial commit

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.Test;
+
+/**
+ * Created by Анна on 03.03.2016.
+ */
+public class TestRunner {
+    @Test
+    public void ThisIsTestMethod1 (){
+        System.out.println("This Is Test Method 1");
+    }
+
+    @Test
+    public void ThisIsTestMethod2 (){
+        System.out.println("This Is Test Method 2");
+    }
+    @Test
+    public void ThisIsTestMethod3 (){
+        System.out.println("This Is Test Method 3");
+    }
+
+
+}
